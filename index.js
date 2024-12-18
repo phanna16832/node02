c3cc0facf86ccf1ecd5681263050be05d2b45975
phanna <soksiphanna35@gmail.com>
2024-12-18 22:09:57 +0700

    update calculate table

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express"); 
-const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
+
+const app = express();
+
+//register ejs
+app.set('view engine', 'ejs');
+
+//middleware static
+app.use(express.static('static'));
+
+app.get("/", (req, res) => { 
+  res.render('home')
+
+ }); 
+
+// 404 Page
+app.use((req, res) => {
+  res.render('404' ,{title : "ahbeoun"}); // Make sure views/home.ejs exists
+});
 
 
-const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+const PORT = process.env.PORT || 5000; app.listen(PORT, () => { 
+  console.log(`Server is running on port http://localhost:${PORT} `);
+ });
+
+
